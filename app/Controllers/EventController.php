695ff10e89153fef13b008413ceefcbecee0d793
yuta4431 <orange_1272@yahoo.co.jp>
2025-11-24 15:49:30 +0900

    イベント編集

--- app/Controllers/EventController.php
+++ app/Controllers/EventController.php
@@ -72,4 +72,42 @@ class EventController
             'event' => $event
         ]);
     }
+
+    public function edit()
+    {
+        $twig = Bootstrap::getTwig();
+
+        $id = $_GET['id'] ?? 0;
+
+        // ダミーデータ
+        $dummyEvents = [
+          1 => ['id' => 1, 'title' => 'ライブ（東京）', 'date' => '2025-11-05', 'memo' => '会場：東京ドーム'],
+          2 => ['id' => 2, 'title' => '配信イベント', 'date' => '2025-11-12 20:00', 'memo' => 'YouTube Live']
+        ];
+
+        $event = $dummyEvents['id'] ?? null;
+
+        echo $twig->render('edit.html.twig', [
+            'event' => $event
+        ]);
+    }
+
+    public function update()
+    {
+        $title = $_POST['title'] ?? '';
+        $date = $_POST['date'] ?? '';
+        $memo = $_POST['memo'] ?? '';
+        $id = $_POST['id'] ?? 0;
+
+        $twig = Bootstrap::getTwig();
+
+        echo $twig->render('show.html.twig', [
+            'event' => [
+                'id' => $id,
+                'title' => $title,
+                'date' => $date,
+                'memo' => $memo
+            ]
+        ]);
+    }
 }
