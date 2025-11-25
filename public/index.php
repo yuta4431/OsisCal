8d53f8611041408dd6eedbb784d10ddf53984a7d
yuta4431 <orange_1272@yahoo.co.jp>
2025-11-26 03:13:40 +0900

    DB・テーブル作成

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $route = $_GET['route'] ?? 'calendar/index';
 list($controllerName, $method) = explode('/', $route);
 
-$controllerClass = "App\\Controllers\\" . ucfirst($controllerName) . "Controller";
+$controllerClass = "App\\Controllers\\" . $controllerName . "controller";
 $controller = new $controllerClass();
 
 $controller->$method();
